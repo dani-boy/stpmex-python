7889ce1060a6adfe1db580237ef10e2ca4b8c0fd
Matin Tamizi <matin@cuenca.io>
2018-08-16 06:24:15 -0500

    use local wsdl

--- stpmex/soap.py
+++ stpmex/soap.py
@@ -3,5 +3,4 @@ import os
 import zeep
 
 
-actualiza_client = zeep.Client(os.environ['STP_ACTUALIZA_WSDL'])
-consulta_client = zeep.Client(os.environ['STP_CONSULTA_WSDL'])
+actualiza_client = zeep.Client('./SpeiServices.wsdl')
