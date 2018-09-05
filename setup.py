4b25beb64d8ef64ff0a808c7e1534355620a9f1d
Matin Tamizi <matin@cuenca.io>
2018-09-05 18:45:07 -0500

    push to pypi. close #1

---
+++ setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+
+setuptools.setup(
+    name='stpmex',
+    version='0.0.1',
+    author='Cuenca',
+    author_email='dev@cuenca.io',
+    description='Integration to stpmex.com',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/cuenca-mx/stpmex-python',
+    packages=setuptools.find_packages(),
+    extras_require={
+        'dev': [
+            'pytest>=3'
+        ]
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ]
+)
