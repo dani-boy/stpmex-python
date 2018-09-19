7fd4533ea7140dc0bf0397175d0e254fa72483af
Ricardo Sanchez <ricardo_8990@hotmail.com>
2018-09-19 10:42:44 -0500

    Update version to pypi

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open('README.md', 'r') as f:
 
 setuptools.setup(
     name='stpmex',
-    version='0.0.3',
+    version='0.0.4',
     author='Cuenca',
     author_email='dev@cuenca.io',
     description='Integration to stpmex.com',
