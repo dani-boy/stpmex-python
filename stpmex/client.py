ba4794c48dcc8e255e5541df71009e710eb4af61
Matin Tamizi <matin@cuenca.com>
2019-08-05 00:23:22 -0500

    fix typo

--- stpmex/client.py
+++ stpmex/client.py
@@ -40,6 +40,6 @@ class Client:
 
     def registrar_orden(self, orden: Orden) -> dict:
         soap_orden = self.soap_orden(orden)
-        joined_fileds = join_fields(soap_orden)
-        soap_orden.firma = compute_signature(self._pkey, joined_fileds)
+        joined_fields = join_fields(soap_orden)
+        soap_orden.firma = compute_signature(self._pkey, joined_fields)
         return self.soap_client.service['registraOrden'](soap_orden)
