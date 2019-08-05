8d8860d606ca7fc1416e0dd5861af14ad332fd3c
Matin Tamizi <matin@cuenca.com>
2019-08-04 22:58:25 -0500

    make consistent

--- setup.py
+++ setup.py
@@ -1,52 +1,36 @@
 import setuptools
 
-requirements = [
+install_requires = [
     'zeep>=3.1.0,<3.2.0',
     'pyopenssl>=18.0.0,<18.1.0',
     'clabe>=0.2.1,<0.3.0',
-    'pydantic'
+    'pydantic>=0.31.1<0.32.0'
 ]
+
+test_requires = ['pytest', 'pytest-vcr', 'pycodestyle', 'pytest-cov',
+                 'black', 'isort[pipfile]']
 
 with open('README.md', 'r') as f:
     long_description = f.read()
 
 setuptools.setup(
     name='stpmex',
-    version='1.0.0',
+    version='2.0.0',
     author='Cuenca',
-    author_email='dev@cuenca.io',
+    author_email='dev@cuenca.com',
     description='Integration to stpmex.com',
     long_description=long_description,
     long_description_content_type='text/markdown',
     url='https://github.com/cuenca-mx/stpmex-python',
     packages=setuptools.find_packages(),
-    install_requires=requirements,
+    python_requires='>=3.7',
+    install_requires=install_requires,
     setup_requires=['pytest-runner'],
-    tests_require=[
-        'pytest>=4.2.1,<4.3.0',
-        'vcrpy==2.0.1',
-    ],
-    extras_require={
-        'dev': [
-            'pytest>=3',
-            'ipython',
-            'ipdb',
-            'pycodestyle',
-            'pytest',
-            'vcrpy',
-            'pytest-vcr>=1.0.1',
-            'black',
-            'isort'
-        ]
-    },
+    tests_require=test_requires,
     classifiers=[
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
     ],
-    entry_points={
-        'console_scripts': [
-            'stpmex = stpmex.__main__:main'
-        ]
-    }
 )
