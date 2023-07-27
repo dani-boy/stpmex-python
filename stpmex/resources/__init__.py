87f62031bb0ba9b65cf96baeb9544c4376a77429
gonz <gonzasestopal@gmail.com>
2023-07-27 13:10:19 -0600

    feat: export estado banco

--- stpmex/resources/__init__.py
+++ stpmex/resources/__init__.py
@@ -2,13 +2,14 @@ __all__ = [
     'Banco',
     'CuentaFisica',
     'CuentaMoral',
+    'EstadoBanco',
     'Orden',
     'OrdenV2',
     'Resource',
     'Saldo',
 ]
 
-from .bancos import Banco
+from .bancos import Banco, EstadoBanco
 from .base import Resource
 from .cuentas import CuentaFisica, CuentaMoral
 from .ordenes import Orden, OrdenV2
